5d38f9b9ebe0cef5f44acb74f061556655fe616e
Baker Sentamu <bakersen@gmail.com>
2021-06-16 09:17:26 +0300

    Modified README.md, index.html, boda.js

--- boda.js
+++ boda.js
@@ -5,22 +5,24 @@ var dropOff = document.getElementById('dropoff');
 var pickUpTime = document.getElementById('pickup-time')
 var arrivalTime = document.getElementById('arrivalTime');
 var temperature = document.getElementById('temp');
-var temperature = document.getElementById('temp');
-var inputName = document.getElementById('nameof');
-var dropDowns = document.querySelectorAll('.custom-select')
+var vaccinated = document.getElementById('vaccinated');
+var gender = document.getElementById('gender')
+
+const inputs = [date, pickUp, dropOff, pickUpTime, arrivalTime, temperature, vaccinated, gender]
 
 var error = document.querySelectorAll('.error')
 
 
-const checkEmpty =()=> {
-     if(date.value =='') {
-        error[0].textContent = "Enter Date";
-    }
+const checkEmpty = (input) => {
+     for(let i=0; i<input.length; i++){
+         if(input[i].value==""){
+             input[i].style.border="1px solid red"
+             error[i].textContent = "This field is required"
+         }
+     }
 }
 
-var button = document.querySelector('button')
 
-button.addEventListener('click', checkEmpty)
 
 
 
