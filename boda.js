926052d9cd48754adde2094971474b9a4bbbc161
Mark Lary <laryalec12@gmail.com>
2021-06-18 01:38:34 +0300

    final validation

--- boda.js
+++ boda.js
@@ -5,10 +5,10 @@ var dropOff = document.getElementById('dropoff');
 var pickUpTime = document.getElementById('pickup-time')
 var arrivalTime = document.getElementById('arrivalTime');
 var temperature = document.getElementById('temp');
-var vaccinated = document.getElementById('vaccinated');
+// var vaccinated = document.getElementById('vaccinated');
 var gender = document.getElementById('gender')
 
-const inputs = [date, pickUp, dropOff, pickUpTime, arrivalTime, temperature, vaccinated, gender]
+const inputs = [date, pickUp, dropOff, pickUpTime, arrivalTime, temperature, gender]
 
 var error = document.querySelectorAll('.error')
 
@@ -17,9 +17,10 @@ const checkEmpty = (input) => {
      for(let i=0; i<input.length; i++){
          if(input[i].value==""){
              input[i].style.border="2px solid red"
-            
+            error[i].textContent = 'Please fill field'
          } else {
-            input[i].style.border="2px solid"
+            input[i].style.border="2px solid lightgrey"
+            error[i].textContent = ''
          }
      }
 }
@@ -30,41 +31,42 @@ let nin = document.form.nationalid
 let small = document.querySelectorAll('small')
 let validation =() => {
     let name = /^[A-Za-z]+$/ 
-    if (nameEl.value <= 8) {
-        nameEl.style.border="2px solid"
+    if (nameEl.value <= 8 || (nameEl.value.match(name))) {
+        nameEl.style.border="2px solid lightgrey"
         small[8].innerHTML = ''
     }
-else if(!nameEl.value.match(name)) {
+else {
     nameEl.style.border = '2px solid red'
     small[8].innerHTML = 'Invalid username'
 }
    
 let tel = /^[0-9]+$/
-if (telNum.value <= 10) {
-    telNum.style.border="2px solid"
+if (telNum.value <= 10 || (telNum.value.match(tel))) {
+    telNum.style.border="2px solid lightgrey"
     small[10].innerHTML = ''
-} else if (!telNum.value.match(tel)){
-    telNum.style.border = "2px solid red"
-    small[10].innerHTML = 'Invalid tel number'
+} 
+ else {
+    telNum.style.border="2px solid red"
+    small[10].innerHTML = 'Invaid no.'
  }
 
- let temp = /[0-9a-zA-Z]+$/
+ let temp = /^[0-9a-zA-Z]+$/
  if (temperature.value.match(temp)){
-    temperature.style.border="2px solid"
-    
+    temperature.style.border="2px solid lightgrey"
+    small[5].innerHTML = ''
  }
  else {
      temperature.style.border = "2px solid red"
-     
+     small[5].innerHTML = 'Invalid temperature'
  }
 
-let id = /[0-9a-zA-Z]+$/
-if(nin.value <=14)  {
-    nin.style.border="2px solid"
-    error[9].innerHTML = ''
-} else if(!nin.value.match(id)) {
+let id = /^[0-9a-zA-Z]+$/
+if(nin.value <=14 || (nin.value.match(id)))  {
+    nin.style.border="2px solid lightgrey"
+    small[9].innerHTML = ''
+} else  {
     nin.style.border="2px solid red"
-    error[9].innerHTML = 'Invalid ID no.'
+    small[9].innerHTML = 'Invalid ID'
 }
 
 
