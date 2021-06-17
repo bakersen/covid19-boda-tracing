601fd6e3bedd4265695c75e8596f53753a7fc2bf
Mark Lary <laryalec12@gmail.com>
2021-06-17 17:41:21 +0300

    better validation

--- boda.js
+++ boda.js
@@ -17,36 +17,58 @@ const checkEmpty = (input) => {
      for(let i=0; i<input.length; i++){
          if(input[i].value==""){
              input[i].style.border="2px solid red"
-             error[i].textContent = "This field is required"
+            
          } else {
-            input[i].style.border="2px solid blue"
-            error[i].textContent = ""
+            input[i].style.border="2px solid"
          }
      }
 }
 
 let nameEl = document.form.nameof
 let telNum = document.form.tel
+let nin = document.form.nationalid
 let small = document.querySelectorAll('small')
 let validation =() => {
     let name = /^[A-Za-z]+$/ 
-    if (nameEl.value.match(name)) {
-        nameEl.style.border ="2px solid blue"
+    if (nameEl.value <= 8) {
+        nameEl.style.border="2px solid"
         small[8].innerHTML = ''
     }
-    else{
-        nameEl.style.border = '2px solid red'
-        small[8].innerHTML = 'Invalid username'
-    }
+else if(!nameEl.value.match(name)) {
+    nameEl.style.border = '2px solid red'
+    small[8].innerHTML = 'Invalid username'
+}
+   
 let tel = /^[0-9]+$/
- if (telNum.value.match(tel)){
-     telNum.style.border = "2px solid blue"
-     small[10].innerHTML = ''
+if (telNum.value <= 10) {
+    telNum.style.border="2px solid"
+    small[10].innerHTML = ''
+} else if (!telNum.value.match(tel)){
+    telNum.style.border = "2px solid red"
+    small[10].innerHTML = 'Invalid tel number'
+ }
+
+ let temp = /[0-9a-zA-Z]+$/
+ if (temperature.value.match(temp)){
+    temperature.style.border="2px solid"
+    
  }
  else {
-     telNum.style.border = "2px dotted red"
-     small[10].innerHTML = 'Invalid tel number'
+     temperature.style.border = "2px solid red"
+     
  }
+
+let id = /[0-9a-zA-Z]+$/
+if(nin.value <=14)  {
+    nin.style.border="2px solid"
+    error[9].innerHTML = ''
+} else if(!nin.value.match(id)) {
+    nin.style.border="2px solid red"
+    error[9].innerHTML = 'Invalid ID no.'
+}
+
+
+ 
 }
 
 
